0b08988dd3221eda5924a17a39b449398354a698
Kleonlive <86655044+Kleonlive@users.noreply.github.com>
2021-06-30 17:06:00 +0300

    Update 01_task_dict.py

--- Module3/practice/dict/01_task_dict.py
+++ Module3/practice/dict/01_task_dict.py
@@ -8,3 +8,11 @@ item = {"name": "Кроссовки", "price": "7540.5", "currency": "rub", "cou
 dollar_rate = 74.12
 
 # TODO: your code here
+
+full_price = 0
+
+for key in item:
+    if key == "name":
+        full_price += (float(item.get("price"))/dollar_rate*int(item.get("count")))
+
+print(full_price)
