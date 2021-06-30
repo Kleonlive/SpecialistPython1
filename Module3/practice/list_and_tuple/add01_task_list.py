08401f61c63748c41fc8ed96df4d3dd181299d13
Kleonlive <86655044+Kleonlive@users.noreply.github.com>
2021-06-30 14:36:22 +0300

    Update add01_task_list.py

--- Module3/practice/list_and_tuple/add01_task_list.py
+++ Module3/practice/list_and_tuple/add01_task_list.py
@@ -3,3 +3,13 @@
 # Получить и вывести на экран список координат y для данного уравнения прямой.
 
 # TODO: your code here
+
+x_coord = [-2,-1,0,1,2,3,4,5,6,7]
+
+y_coord = []
+
+for coord in x_coord:
+    y = 2*coord-4
+    y_coord.append(y)
+
+print(y_coord)
