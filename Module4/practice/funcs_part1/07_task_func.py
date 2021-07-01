4de8a99dbde8f36ae2fd238db9405930af09e2b0
Kleonlive <86655044+Kleonlive@users.noreply.github.com>
2021-07-01 12:22:52 +0300

    Update 07_task_func.py

--- Module4/practice/funcs_part1/07_task_func.py
+++ Module4/practice/funcs_part1/07_task_func.py
@@ -2,3 +2,12 @@
 # где hh - часы, mm- минуты, ss - секунды.
 # Пример:
 # 29143 секунд → 08:05:43
+
+def sec_to_time(sec):
+    c_sec = sec % 60
+    c_min = (sec // 60) % 60
+    c_hour = (sec // 60) // 60
+    c_time = f"{c_hour:0>2}:{c_min:0>2}:{c_sec:0>2}"
+    return c_time
+
+print(sec_to_time(29143))
