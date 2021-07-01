31a00a0c40e84224c8854073873ebc6da2c7c32b
Kleonlive <86655044+Kleonlive@users.noreply.github.com>
2021-07-01 12:34:21 +0300

    Update 08_task_func.py

--- Module4/practice/funcs_part1/08_task_func.py
+++ Module4/practice/funcs_part1/08_task_func.py
@@ -1,1 +1,15 @@
 # Напишите функцию находящую n-ое число Фибоначчи.
+
+def fibo(n):
+    num = 1
+    buffer_num = 0
+    prev_num = 0
+    i = 1
+    while i < (n - 1):
+        buffer_num = num
+        num = num + prev_num
+        prev_num = buffer_num
+        i += 1
+    return num
+
+print(fibo(20))
